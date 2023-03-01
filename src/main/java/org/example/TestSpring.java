b575df1b7bf9803532aee9b7320c74239cc26d21
sgorg <sgorg2002@gmail.com>
2023-03-01 15:42:25 +0200

    add Dependency Injection (DI)

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,10 +7,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( "applicationContext.xml");
 
         //TestBean testBean = context.getBean("testBean", TestBean.class);
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         //System.out.println(musicPlayer);
         //System.out.println(music.getMusic());
